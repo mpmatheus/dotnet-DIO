825f8e718a5b5381a36f6db07351acdf1b30fdd2
MPmatheus MpMatheus <mp.matheus02@gmail.com>
2022-02-10 15:06:12 -0300

    ADD aula Read Directory

---
+++ ReadDirectory/Program.cs
@@ -0,0 +1,23 @@
+﻿var path = @"C:\globo";
+lerDiretorios(path);
+Console.WriteLine("aperte [enter] para finalizar");
+Console.ReadLine();
+
+static void lerDiretorios(String path)
+{
+    if(Directory.Exists(path))
+    {
+        var diretorio = Directory.GetDirectories(path,"*", SearchOption.AllDirectories);
+        foreach (var dir in diretorio)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+            Console.WriteLine($"[Nome] : {dirInfo.Name}");
+            Console.WriteLine($"[Raiz] : {dirInfo.Root}");
+            if (dirInfo.Parent != null)
+                Console.WriteLine($"[Pai] : {dirInfo.Parent.Name}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+    else
+        Console.WriteLine($"{path} não encontrado");
+}
