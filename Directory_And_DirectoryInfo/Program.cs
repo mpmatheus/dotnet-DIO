331f2559fd349d0f95281551328e7e3c33f8e602
MPmatheus MpMatheus <mp.matheus02@gmail.com>
2022-02-10 13:36:42 -0300

    ADD aula Directory

---
+++ Directory_And_DirectoryInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var path = Path.Combine (Environment.CurrentDirectory, "globo");
+var dirGlobo = Directory.CreateDirectory(path);
+var dirAMNorte = dirGlobo.CreateSubdirectory ("América do Norte");
+var dirAMCentral = dirGlobo.CreateSubdirectory ("América Central");
+var dirAMSul = dirGlobo.CreateSubdirectory ("América do Sul");
+dirAMNorte.CreateSubdirectory("USA");
+dirAMNorte.CreateSubdirectory("Mexico");
+dirAMNorte.CreateSubdirectory("Canada");
+
+dirAMCentral.CreateSubdirectory("Costa Rica");
+dirAMCentral.CreateSubdirectory("Panama");
+
+dirAMSul.CreateSubdirectory("Brasil");
+dirAMSul.CreateSubdirectory("Argentina");
+dirAMSul.CreateSubdirectory("Paraguai");
