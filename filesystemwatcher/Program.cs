b6397ba181d8d59b0262ba14aa80487b66cca215
MPmatheus MpMatheus <mp.matheus02@gmail.com>
2022-02-14 10:11:07 -0300

    ADD aula File System Watcher

---
+++ filesystemwatcher/Program.cs
@@ -0,0 +1,29 @@
+﻿var path = @"C:";
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += onCreated;
+fsw.Renamed += onRename;
+fsw.Deleted += onDelete;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine($"Monitorando {path}");
+Console.WriteLine("Aperte [enter] para sair...");
+Console.ReadLine();
+void onRename(object sender, RenamedEventArgs e)
+{
+   Console.WriteLine($"o arquivo {e.OldName} foi alterado para{e.Name} ");
+}
+
+
+
+void onDelete(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi deletado o arquivo: {e.Name}");
+}
+
+
+void onCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi criado um arquivo: {e.Name}");
+}
